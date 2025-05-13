5722ba23c1cca5f7e186bea52249da64c4362a00
lilprof01 <adewumime@gmail.com>
2025-05-13 00:06:13 +0000

    stuck

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>ALX Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - ALX</p>');
+
+$('button').on('click', _.debounce(updateCounter, 500));
+
+let count = 0;
+
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
